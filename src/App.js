635a331b6f37e167cd4703efa15b540c1d5365a3
Rutkay Biyik <rutkay@scanbot.io>
2022-03-22 11:48:39 +0100

    barcode scanner example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React from 'react';
+import ScanbotSDK from 'scanbot-web-sdk/webpack';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+  barcodes = [];
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lastBarcode: null,
+    }
+  }
+
+  async componentDidMount() {
+    this.sdk = await ScanbotSDK.initialize({
+      licenseKey: '',
+      engine: "/",
+    });
+
+    const config = {
+      onBarcodesDetected: this.onBarcodesDetected.bind(this),
+      containerId: 'barcode-scanner-view',
+      style: {
+        window: {
+          aspectRatio: 2,
+          paddingPropLeft: 0.7,
+        },
+      },
+      barcodeFormat: [
+        "AZTEC",
+        "CODABAR",
+        "CODE_39",
+        "CODE_93",
+        "CODE_128",
+        "DATA_MATRIX",
+        "EAN_8",
+        "EAN_13",
+        "ITF",
+        "MAXICODE",
+        "PDF_417",
+        "QR_CODE",
+        "RSS_14",
+        "RSS_EXPANDED",
+        "UPC_A",
+        "UPC_E",
+        "UPC_EAN_EXTENSION",
+        "MSI_PLESSEY"
+      ]
+    };
+
+    this.barcodeScanner = await this.sdk.createBarcodeScanner(config);
+
+  }
+
+  componentWillUnmount() {
+    this.barcodeScanner.disposebBarcodeScanner();
+  }
+
+  render() {
+    let barcodeText;
+    if (!this.state.lastBarcode) {
+      barcodeText = '';
+    } else {
+      const barcodes = this.state.lastBarcode.barcodes;
+      barcodeText = JSON.stringify(
+        barcodes.map((barcodes) => barcodes.text + " (" + barcodes.format + ") "));
+    }
+
+    return (
+      <div>
+        <div
+          id='barcode-scanner-view'
+          style={{ height: "50%", width: "50%" }}>
+        </div>
+        {barcodeText}
+      </div>
+    );
+  }
+
+  async onBarcodesDetected(result) {
+    this.barcodes.push(result);
+    this.setState({
+      lastBarcode: result
+    });
+  }
+}
